7fb8cdb858fab0db1da059ad4be8c9d6d726f54e
Sonia Hussain <sonia_ahm@icloud.com>
2023-09-05 22:09:16 +0100

    Refactor JS folder structure by adding custom JS files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,10 +2,4 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('bm'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'https://lottie.host/29bb7105-ced3-4178-88a2-f799e0361401/vQ08u7baXF.json'
-})
+import "custom/main"
---
+++ app/javascript/custom/main.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://lottie.host/29bb7105-ced3-4178-88a2-f799e0361401/vQ08u7baXF.json'
+})
